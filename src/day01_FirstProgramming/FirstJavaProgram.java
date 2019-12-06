d1762abb31ae27d6b7e782cef28fb20f56dcc9b7
Afgan Akbarov <ekberli87@gmail.com>
2019-12-05 23:42:34 -0500

    aa

--- src/day01_FirstProgramming/FirstJavaProgram.java
+++ src/day01_FirstProgramming/FirstJavaProgram.java
@@ -8,7 +8,7 @@ public class FirstJavaProgram {
 		System.out.println("Hello World");
 		
 		System.out.println("Hello Cybertek Batch 12");
-		System.out.println("Hello World");
+		System.out.println("Hello Yasin");
 	}
 	
 	
